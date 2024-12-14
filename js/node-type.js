b6a459b797d73e1fbd170b214cc8441d776192bc
Korey Hinton <2801399+koreyhinton@users.noreply.github.com>
2024-12-14 08:47:30 -0500

    0.4 docs, game flow type fn stub

--- js/node-type.js
+++ js/node-type.js
@@ -18,3 +18,31 @@ window.tyIsDecisionNd = function(nd) {
     var l = points.length;
     return points[0] == points[l-2] && points[1] == points[l-1];
 }
+
+window.tyIsGameFlowRect = function(nd) {
+    // nd input must be a polyline or an error might be thrown?
+    var points = nd.attrs.filter(a => a.name == "points")[0].value.split(" ");
+
+    // last point at index 12 == first point at index 0
+    //
+    //     1_______________2
+    //    0|               |3 4
+    //  11|   DARKER TEXT    |
+    //    |   My Game Node   |
+    //  10|_   ^lighter     _|
+    //     9|______________|6 5
+    //      8              7
+
+    if (points.length !== 13)
+        return false;
+
+    var zeroPoint = points[0];
+    var twelfthPoint = points[12];
+
+    if (zeroPoint !== twelfthPoint)
+        return false;
+
+    // todo: additional checks
+
+    return true;
+}
